26ef7f4b12898e617d76850732cd8399f236d75b
maxdenstore <31626964+maxdenstore@users.noreply.github.com>
2018-05-09 13:58:41 +0200

    addded proj

---
+++ DABHandinGr10/SGT_DocDB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGT_DocDB
+{
+    public class Class1
+    {
+    }
+}
