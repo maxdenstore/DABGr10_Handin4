1da38a0ce4511ca3e273475a18f32a21b37a4be5
nicho1991 <ladefoged1991@gmail.com>
2018-05-13 20:22:11 +0200

    renaming assembly , namesspaces and dir

--- DABHandinGr10/VillageSqlDB/Models/Village.cs
+++ DABHandinGr10/VillageSqlDB/Models/Village.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DABHandinGr10
+namespace VillageSqlDB.Models
 {
-    public class Class1
+    public class Village
     {
     }
 }
