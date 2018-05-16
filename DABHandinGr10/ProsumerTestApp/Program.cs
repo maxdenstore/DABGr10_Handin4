f0a0d4fa3237133efbc3ad1dfdfe7248bdefb6d8
friis94 <nanna_friis@live.dk>
2018-05-16 14:18:20 +0200

    .

---
+++ DABHandinGr10/ProsumerTestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProsumerTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prosumer = new ProsumerDocDB.Models.Prosumer();
+            prosumer.copperId = 1;
+            prosumer.smartmeter = 10;
+            prosumer.wallet = 100;
+            prosumer.prosumerType = "private";
+
+            ProsumerDocDB.DbContext.ProsumerDocumentDBUnitOfWork x = new ProsumerDocDB.DbContext
+                .ProsumerDocumentDBUnitOfWork(new ProsumerDocDB.DbContext.ProsumerDbContext());
+
+            x._prosumerRepository.AddProsumer(prosumer).Wait();
+
+        }
+    }
+}
