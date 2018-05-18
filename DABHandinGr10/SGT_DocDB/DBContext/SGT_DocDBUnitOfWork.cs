9470485220584e3d11f92cff91bb5a76917c96c8
friis94 <nanna_friis@live.dk>
2018-05-18 08:24:49 +0200

    Start on SGT Repository

---
+++ DABHandinGr10/SGT_DocDB/DBContext/SGT_DocDBUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGT_DocDB.DBContext
+{
+    class SGT_DocDBUnitOfWork
+    {
+        public SGT_DocDB.Repository.SGT_Repository _SGT_Repository;
+
+        private readonly SGT_DocDB.DBContext.SGTDBContext _context;
+
+        public SGT_DocDBUnitOfWork(SGT_DocDB.DBContext.SGTDBContext context)
+        {
+            _context = context;
+            _SGT_Repository = new SGT_DocDB.Repository.SGT_Repository(_context);
+        }
+    }
+}
---
+++ DABHandinGr10/SGT_DocDB/Repository/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGT_DocDB.Repository
+{
+    public class Repository<T> where T : class
+    {
+        protected readonly SGT_DocDB.DBContext.SGTDBContext _Context;
+
+        public Repository(SGT_DocDB.DBContext.SGTDBContext context)
+        {
+            _Context = context;
+        }
+    }
+}
